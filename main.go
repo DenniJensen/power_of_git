f274324f4855e9938a7ef809f5b18e00825dc17c
Dennis Haegler <dennis.haegler@gmail.com>
2018-01-30 23:47:45 +0100

    Log errors on webserver

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -10,5 +11,5 @@ func main() {
 		fmt.Fprintf(w, "Hello World")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
